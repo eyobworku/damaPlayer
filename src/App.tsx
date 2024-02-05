29b5cd752230a06413b22ed35de46c153ec87ba2
eyob_worku <eyobworku62@gmail.com>
2024-02-05 12:11:44 +0300

    latest

--- src/App.tsx
+++ src/App.tsx
@@ -5,46 +5,46 @@ import SideBar from "./components/SideBar";
 import { useState } from "react";
 
 function App() {
-  const [eftaState, setEftaState] = useState(false);
-  const [currentPlayer, setCurrentPlayer] = useState(0);
-  return (
-    <>
-      <Grid
-        templateAreas={`'nav'
+	const [eftaState, setEftaState] = useState(false);
+	const [currentPlayer, setCurrentPlayer] = useState(0);
+	return (
+		<>
+			<Grid
+				templateAreas={`'nav'
                         'main side'`}
-        gridTemplateRows={"50px auth"}
-        gridTemplateColumns={"auth 60px"}
-      >
-        <GridItem area={"nav"} gridRow="1" gridColumn="1 / span 2">
-          <NavBar />
-        </GridItem>
-        <GridItem area={"main"} gridColumn="1">
-          <Board
-            checkEftaVar={eftaState}
-            offSelectEfta={() => {
-              setEftaState(false);
-            }}
-            updateCurentPlaying={(curPl) => {
-              setCurrentPlayer(curPl);
-            }}
-          />
-        </GridItem>
-        <GridItem
-          marginY={3}
-          bg="gray"
-          area={"side"}
-          gridColumn="2"
-          gridRow="2"
-        >
-          <SideBar
-            eftaState={eftaState}
-            currentPlayer={currentPlayer}
-            onSelectEfta={() => setEftaState(!eftaState)}
-          />
-        </GridItem>
-      </Grid>
-    </>
-  );
+				gridTemplateRows={"60px 1fr"}
+				gridTemplateColumns={"1fr 300px"}
+			>
+				<GridItem area={"nav"} gridRow="1" gridColumn="1 / span 2">
+					<NavBar />
+				</GridItem>
+				<GridItem area={"main"} gridColumn="1" gridRow="2">
+					<Board
+						checkEftaVar={eftaState}
+						offSelectEfta={() => {
+							setEftaState(false);
+						}}
+						updateCurentPlaying={(curPl) => {
+							setCurrentPlayer(curPl);
+						}}
+					/>
+				</GridItem>
+				<GridItem
+					marginY={3}
+					bg="gray"
+					area={"side"}
+					gridColumn="2"
+					gridRow="2"
+				>
+					<SideBar
+						eftaState={eftaState}
+						currentPlayer={currentPlayer}
+						onSelectEfta={() => setEftaState(!eftaState)}
+					/>
+				</GridItem>
+			</Grid>
+		</>
+	);
 }
 
 export default App;
